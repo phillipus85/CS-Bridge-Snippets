4e340503b8bb99285bbf3e5de2fcd9f52a473d63
Felipe Martin <4166437+phillipus85@users.noreply.github.com>
2025-08-20 19:02:53 -0500

    fix: Update module summary and improve structure for list concepts in Python

--- pylatino/data/list/04 - func_list_v2.py
+++ pylatino/data/list/04 - func_list_v2.py
@@ -1,11 +1,12 @@
 ﻿"""
-Este modulo es el codigo guia para los conceptos de lista de pylatino 2025.
+Este modulo es el codigo guia para los recorridos de lista de pylatino 2025.
 
 RESUMEN DEL MÓDULO
     Este módulo es una introducción a las listas en Python e incluye:
         - PARTE 1: Definir listas con elementos.
         - PARTE 2: Relacionar con otros comandos python.
         - PARTE 3: Recorrer elementos de una lista.
+        - PARTE 4: Funciones con listas.
 NOTAS:
     - No hay elementos duplicados en una lista.
     - Las listas son mutables, lo que significa que se pueden modificar después de su creación.
@@ -24,92 +25,32 @@ NOTAS:
 def main():
     """main es la funcion principal del programa
     """
-    print("===== Introducción a listas =====")
+    print("===== Recorridos de listas =====")
 
-    # TODO PARTE 1: Definir listas, vacias y con elementos.
-    # definir lista vacia
-    print("--- Definición de listas ---\n")
-    repisa = []
-    sinonimo = list()
-    # verificar que ambos tipos de creacion son equivalentes
-    print("son listas? ->", repisa == sinonimo)
-    # pero no son la misma variable
-    print("son la misma variable? ->", repisa is sinonimo, "\n")
-
+    # TODO PARTE 1: Definir lista con elementos.
+    print("\n--- PARTE 1: Definiendo listas con elementos. ---")
+    # creando una lista con elementos, repisa con libros!
     repisa = [
         "Rayuela, Julio Cortázar",
         "Ficciones, Jorge Luis Borges",
         "Aura, Carlos Fuentes",
         "Delirio, Laura Restrepo",
+        "El túnel, Ernesto Sabato",
+        "La hojarasca, Gabriel García Márquez",
+        "Cien años de soledad, Gabriel García Márquez"
     ]
 
+    # creando lista con elementos con list()
     sinonimo = list(repisa)
-    print("son listas? ->", repisa == sinonimo)
-    print("son la misma variable? ->", repisa is sinonimo, "\n")
+    # verificar que ambas listas son iguales
+    print("\trepisa':", repisa, "'sinonimo':", sinonimo)
+    print("\tSon listas iguales y con elementos?", repisa == sinonimo)
 
-    # # lista con solo titulos
-    # titulos = [
-    #     "Rayuela",
-    #     "Ficciones",
-    #     "Aura",
-    #     "Delirio",
-    # ]
-
-    # # lista con solo autores
-    # autores = [
-    #     "Julio Cortázar",
-    #     "Jorge Luis Borges",
-    #     "Carlos Fuentes",
-    #     "Laura Restrepo",
-    # ]
-
-    # TODO PARTE 2: Acceder a elementos de una lista.
-    print("--- Acceder a elementos de una lista ---")
-    # acceso a elementos segun su posicion
-    # primer elemento
-    primer_libro = repisa[0]
-    print("Primer libro:", primer_libro)
-    # último elemento
-    # opción 1
-    ultimo_libro = repisa[-1]
-    print("Último libro (opción 1):", ultimo_libro)
-    # opción 2, sinonimo con len
-    ultimo_libro = repisa[len(repisa) - 1]
-    print("Último libro (opción 2):", ultimo_libro, "\n")
-
-    # TODO PARTE 3: Agregar y eliminar elementos de una lista.
-    print("--- Agregar y eliminar elementos de una lista ---")
-
-    # agregar un libro
-    # opción 1, append, última posición
-    nuevo_libro = "El túnel, Ernesto Sabato"
-    repisa.append(nuevo_libro)
-    repisa.append(nuevo_libro)
-    print("Después de agregar un libro:\n", repisa)
-
-    # opción 2, insert, posición específica
-    nuevo_libro = "La hojarasca, Gabriel García Márquez"
-    indice = 1
-    repisa.insert(indice, nuevo_libro)
-    print("Después de agregar un libro en posición", indice, ":\n", repisa)
-
-    # eliminar un libro
-    # opción 1, pop, última posición
-    libro = repisa.pop()
-    print("Después de eliminar un libro:\n", repisa)
-    print("Libro eliminado:", libro)
-
-    # opción 2, remove, por valor
-    # eliminar un libro por su nombre
-    titulo = "Ficciones, Jorge Luis Borges"
-    # que pasa si no es exactamente igual
-    # libro = "ficciones, Jorge Luis Borges"
-    libro = repisa.remove(titulo)
-    print("Después de eliminar un libro:\n", repisa)
-    print("Libro eliminado:", titulo, "\n")
-
-    # TODO PARTE 4: relacionar con otros comandos python
+    # TODO PARTE 2: relacionar con otros comandos python
+    print("\n--- PARTE 2: Relacionar con otros comandos python ---")
     # uso de condicionales
+    print("\tRepisa:", repisa)
+
     # revisar si un elemento esta en la lista con in
     libro = "Delirio, Laura Restrepo"
     esta = (libro in repisa)
@@ -119,71 +60,78 @@ def main():
     # if esta is True:
     # if esta is not False:
     # if libro in repisa is True:
-    # if libro in repisa is not False
+    # if libro in repisa is not False:
     if esta:
-        print("El libro ", libro, "está en la repisa.")
+        print("\n\tEl libro ", libro, "está en la repisa.")
     else:
-        print("El libro no está en la repisa.")
+        print("\n\tEl libro ", libro, "no está en la repisa.")
 
-    # TODO PARTE 5: Modificar elementos de una lista.
-    print("\n--- Modificar elementos de una lista ---")
-    libro_viejo = repisa[0]
-    libro_nuevo = "Cien años de soledad, Gabriel García Márquez"
-    repisa[0] = libro_nuevo
+    # libro que no esta
+    otro_libro = "Cien años de soledad, Gabriel García Márquez"
+    # esta = (otro_libro in repisa)
+    if otro_libro in repisa is True:
+        print("\tEl libro ", otro_libro, "está en la repisa.")
+    else:
+        print("\tEl libro ", otro_libro, "no está en la repisa.")
 
-    print(libro_viejo, "ha sido reemplazado por", libro_nuevo)
-    print(libro_viejo in repisa)
-    print(repisa)
+    # TODO PARTE 3: Recorrer elementos de una lista.
+    print("\n--- PARTE 3: Recorrer elementos de una lista ---")
+    print("\tRepisa:", repisa)
 
-    # TODO PARTE 6: Recorrer elementos de una lista.
-    print("--- Recorrer elementos de una lista ---")
     # opción 1, usando el iterador for
-    print("\nRecorriendo con iterador for (opción 1):")
+    print("\n\tRecorriendo con iterador for (opción 1):")
     for libro in repisa:
-        print("\tEn la repisa está:", libro)
+        print("\t\tEn la repisa está:", libro)
 
     # opción 2, usando el índice
-    print("\nRecorriendo con índice (opción 2):")
+    print("\n\tRecorriendo con índice (opción 2):")
     for i in range(len(repisa)):
-        print("\tEn la repisa está:", repisa[i])
+        print("\t\tEn la repisa está:", repisa[i])
 
-    # opción 3, usando enumerate
-    print("\nRecorriendo con enumerate (opción 3):")
-    for i, libro in enumerate(repisa):
-        print("\tEn la repisa está:", libro, "(índice", i, ")")
+    # opción 2, utilizando el indice para modificar datos
+    print("\n\tModificando con índice (opción 2):")
+    for i in range(len(repisa)):
+        print("\t\tEn la repisa está:", repisa[i])
+        # modificar el libro a mayúsculas
+        libro = repisa[i]
+        libro = libro.upper()
+        repisa[i] = libro
+        # # alternativa 1
+        # libro = repisa[i]
+        # repisa[i] = libro.upper()
+        # # alternativa 2
+        # repisa[i] = repisa[i].upper()
 
-    # opción 4, while
+    # revisar cambios
+    print("\n\tRepisa:", repisa)
+
+    # reseteando cambios
+    repisa = list(sinonimo)
+
+    # opción 3, while con contador
     i = 0
-    print("\nRecorriendo con while (opción 4):")
+    print("\nRecorriendo con while (opción 3):")
     while i < len(repisa):
-        print("\tEn la repisa está:", repisa[i])
+        print("\t\tEn la repisa está:", repisa[i])
         i += 1
 
-    # bono recorerlo al revez
-    print("\nRecorriendo al revés con indice (opción 5):")
-    for i in range(len(repisa) - 1, -1, -1):
-        print("\tEn la repisa está:", repisa[i])
-        i -= 1
+    # opcion 3, cambiar con while y condicional.
+    # libro importante y prestado
+    libro_importante = "Delirio, Laura Restrepo"
+    libro_prestar = "Cien años de soledad, Gabriel García Márquez"
 
-    # bono recorerlo al revez con iterador
-    print("\nRecorriendo al revés con iterador (opción 6):")
-    for libro in reversed(repisa):
-        print("\tEn la repisa está:", libro)
+    i = 0
+    while i < len(repisa):
+        if repisa[i] == libro_importante:
+            repisa[i] = repisa[i].upper()
+        if repisa[i] == libro_prestar:
+            prestado = repisa[i]
+            print("\t\tLibro prestado:", prestado)
+            repisa.pop(i)
+        i += 1
 
-    # bono recorrerlo al revez con while
-    print("\nRecorriendo al revés con while (opción 7):")
-    i = len(repisa) - 1
-    while i >= 0:
-        print("\tEn la repisa está:", repisa[i])
-        i -= 1
-
-    # repisa.append("Ficciones, Jorge Luis Borges")
-    # repisa.append("Rayuela, Julio Cortázar")
-    # repisa.append("Delirio, Laura Restrepo")
-    # repisa.append("Delirio, Laura Restrepo")
-    # # libro = "Delirio, Laura Restrepo"
-    # libro = "Rayuela, Julio Cortázar"
-    # conteo = contar_libros(repisa, libro)
+    # revisar cambios
+    print("\n\tRepisa:", repisa)
 
 
 if __name__ == '__main__':
